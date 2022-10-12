836c9eb417780ba6dcf37ae572da4113dd00130b
VlRsMD <90247966+VlRsMD@users.noreply.github.com>
2022-10-12 05:35:54 +0300

    Update main.py
    Create dinning-hall server and send generated orders to kitchen server

--- main.py
+++ main.py
@@ -1,1 +1,65 @@
+from datetime import datetime
+import random
+import flask
+import time
+import threading
+import requests
+import json
+import menu
+import order_out
 
+DH_App = flask.Flask(__name__)
+st = []
+
+@DH_App.put("/put")
+
+def order_stack():
+    global st
+    ## generating total of 40 orders
+    for k in range(4):
+        ## 10 tables generating orders
+        for i in range(10):
+            order_id = random.randrange(1, 11, 0)
+            table_id = i
+            waiter_id = random.randrange(1, 5, 0)
+            priority = random.randrange(1, 6, 0)
+            items = [random.randrange(1, 11, 0), random.randrange(1, 11, 0), random.randrange(1, 11, 0)]
+            ## calculating maximum waiting time (5 next lines)
+            max = menu.menu[items[0]].prep_time
+            for i in range(1, 3):
+                if menu.menu[items[i]].prep_time > max:
+                    max = menu.menu[items[i]].prep_time
+            max_wait = max * 1.3
+            pick_up_time = datetime.timestamp(datetime.now())
+            new_order = order_out.orderOut(order_id, table_id, waiter_id, priority, items, max_wait, pick_up_time)
+            ## adding new order to the stack
+            st.append(new_order)
+        ## sleep for a while so that a table with same id does not generate orders with no pause
+        time.sleep(1)
+
+## send orders to kitchen
+def send_order():
+    global st
+    mutex = threading.Lock()
+    ## do while stack is not empty
+    while len(st) > 0:
+        ## lock
+        mutex.acquire()
+        pick_up_order = st.pop()
+        order_to_json = json.dumps(pick_up_order.__dict__)
+        requests.put("http://localhost:3501/put", json=order_to_json)
+        ## unlock
+        mutex.release()
+    ## notify kitchen that no more orders are coming
+    if len(st) == 0:
+        requests.put("http://localhost:3501/put", json={"state": "no more incoming orders"})
+
+## multiple waiters threads
+waiters = [threading.Thread(target=send_order) for i in range(4)]
+
+if __name__ == '__main__':
+    ## starting waiters multithreading
+    for waiter in waiters:
+        waiter.start()
+    ## starting dinning hall server
+    DH_App.run(host='localhost', port=3500)
